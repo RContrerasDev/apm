f0c44210a91640e5e927bb43914629388052968d
RContrerasDev <rcarlos_cr@yahoo.com.mx>
2018-11-18 19:53:22 -0600

    first commit

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from "@angular/core";
+import { IProduct } from "./product";
+import { ProductService } from "./product.service";
+
+// Next we decorate the class with a Component decorator, it is what makes this class a component.
+// The decorator will hold the properties and also defines the metadata.
+@Component({
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html',
+    styleUrls: ['./product-list.component.css']
+})
+
+// First we create the class and export so it is available to the other parts of the application.
+export class ProductListComponent implements OnInit {
+    pageTitle: string = 'Product List';
+    imageWidth: number = 50;
+    imageMargin: number = 2;
+    showImage: boolean = false;
+    filteredProducts: IProduct[];
+    _listFilter: string;
+    errorMessage: string;
+
+    get listFilter(): string {
+        return this._listFilter;
+    }
+    set listFilter(value:string) {
+        this._listFilter = value;
+        this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
+    }
+
+    products: IProduct[] = [];
+
+    constructor(private productService: ProductService) {     
+
+    }
+
+    onRatingClicked(message: string): void {
+        this.pageTitle = 'Product List: ' + message;
+    }
+
+    performFilter(filterBy: string): IProduct[] {
+        filterBy = filterBy.toLocaleLowerCase();
+        return this.products.filter((product:IProduct) =>
+                product.productName.toLocaleLowerCase().indexOf(filterBy) !== -1);
+    }
+
+    toggleImage(): void {
+        this.showImage = !this.showImage;
+    }
+
+    ngOnInit(): void {
+        console.log('In OnInit() method');
+        this.productService.getProducts().subscribe(
+            products => {
+                this.products = products;
+                this.filteredProducts = this.products;
+            },
+            error => this.errorMessage = <any>error
+        );
+    }
+}
